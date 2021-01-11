348a0eb09650691b785035bd99d1e064764d6252
dineshRJ <dinesh@moonraft.com>
2021-01-11 22:21:06 +0530

    Added psuedo element example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import GridExample from "./components/GridExample/GridExample";
-import PositionExample from "./components/PositionExample/PositionExample";
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GridExample from './components/GridExample/GridExample';
+import PositionExample from './components/PositionExample/PositionExample';
+import PsuedoElements from './components/PsuedoElements/PsuedoElements';
 
 function App() {
-  return <PositionExample />;
+  return <PsuedoElements />;
 }
 
 export default App;
---
+++ src/components/PsuedoElements/PsuedoElements.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './PsuedoElements.css';
+
+const PsuedoElements = () => {
+  return (
+    <div className='card'>
+      Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+      laying out print, graphic or web designs. The passage is attributed to an
+      unknown typesetter in the 15th century who is thought to have scrambled
+      parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen
+      book.
+    </div>
+  );
+};
+
+export default PsuedoElements;
